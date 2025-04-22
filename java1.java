c5c1afdab1271130205616652d9bce5f8f15ff67
DarshanP251 <darshanpatil0511@gmail.com>
2025-04-22 12:13:37 +0530

    save

--- java1.java
+++ java1.java
@@ -5,6 +5,7 @@ public class java1 {
     public static void main(String[] args) {
         System.out.println("MAIN");
         System.out.println("MASTER");
+        System.out.println("MASTER NEW");
     }
     
 }
